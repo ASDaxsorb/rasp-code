fc9e869a2e5a45aacabffe04aad85d87f84ccb8b
Axell Solis <axellsolisrubio@gmail.com>
2022-09-06 01:31:33 -0500

    Prevent cache

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
